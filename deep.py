e924acadea45741d62e11cecce1805c894763ec3
Rafiyad Darazat <141349190+rafiyad@users.noreply.github.com>
2023-09-23 17:34:46 +0600

    Add files via upload

---
+++ deep.py
@@ -0,0 +1,6 @@
+name=str(input("What is the Answer to the Great Question of Life, the Universe, and Everything?")).lower().strip()
+
+if name=="forty-two" or name=="42" or name=="forty two":
+    print("Yes")
+else:
+    print("No")
