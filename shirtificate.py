7aef9ba021582b6036debb45767e27e36b7775b1
Rafiyad Darazat <141349190+rafiyad@users.noreply.github.com>
2023-12-21 22:49:29 +0600

    Add files via upload

---
+++ shirtificate.py
@@ -0,0 +1,24 @@
+from fpdf import FPDF
+
+class PDF():
+    def __init__(self, name):
+        self._pdf= FPDF()
+        self._pdf.add_page()
+        self._pdf.set_font("helvetica", "B", 15)
+        self._pdf.cell(0, 50, "CS50 shirtificate", new_x="LMARGIN", new_y="NEXT", align='C')
+        self._pdf.image("shirtificate.png", w=self._pdf.epw)
+        self._pdf.set_font_size(28)
+        self._pdf.set_text_color(255, 255, 255)
+        self._pdf.text(x=48, y=140, txt=f"{name} took CS50")
+
+
+
+    def save(self, name):
+        self._pdf.output(name)
+
+
+
+
+name= input("Name: ")
+pdf = PDF(name)
+pdf.save("shirtificate.pdf")
